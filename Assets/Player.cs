8a5ca435c4e3309ce7a14e539224a2f024bf572c
Daniel Purdes <danielpurdes@wireless-10-145-247-211.public.utexas.edu>
2015-09-12 13:17:08 -0500

    added player, no sprite

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControl : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	private float moveSpeed = 3.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		gameObject.AddComponent<CircleCollider2D> ();
+		gameObject.AddComponent<Rigidbody2D> ();
+		gameObject.AddComponent<SpriteRenderer> ();
+
+		CircleCollider2D cc = gameObject.GetComponent<CircleCollider2D> ();
+		cc.radius = .25f;
+
+		Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D> ();
+		rb.gravityScale = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,7 +28,7 @@ public class CameraControl : MonoBehaviour {
 			transform.Translate (0, -moveSpeed * Time.deltaTime, 0);
 		if (Input.GetKey ("w"))
 			transform.Translate (0, moveSpeed * Time.deltaTime, 0);
-
+		
 		if (Input.GetKey ("j"))
 			transform.Rotate (0, 0, 60 * Time.deltaTime);
 		if (Input.GetKey ("l"))
